131c5dfd1bb947a42ef9180fc49060bc3874a39f
farmanali11 <farmanalishar11@gmail.com>
2025-06-29 19:20:08 +0500

    user can send and recieve the message now

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -6,8 +6,6 @@ export const getUsersforSidebar = async (req,res)=>{
     try{
         const userId = req.user._id;
         const filteredUsers = await User.find({_id:{$ne:userId}}).select("-password")
-
-
         const unseenMessages = {}
         const promises = filteredUsers.map(async(user)=>{
             const messages = await Message.find({senderId:user._id,receiverId:userId,seen:false})
@@ -17,8 +15,6 @@ export const getUsersforSidebar = async (req,res)=>{
             }
 
         })
-
-
         await Promise.all(promises)
         res.json({success:true,users:filteredUsers,unseenMessages})
 
@@ -27,4 +23,44 @@ export const getUsersforSidebar = async (req,res)=>{
         console.log(error.message)
         res.json({success:false,error:error.message})
     }
+}
+
+//Get all messages for selected user
+
+export const getMessages = async (req,res)=>{
+    try{
+        const {id:selectedUserId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or:[
+                {senderId:myId,receiverId:selectedUserId},
+                {senderId:selectedUserId,receiverId:myId}
+            ]
+        })
+        await Message.updateMany({senderId:selectedUserId,receiverId:myId},
+        {seen:true})
+        res.json({success:true,messages})
+
+    }catch(error){
+        console.log(error.message)
+        res.json({success:false,error:error.message})
+    }
+}
+
+// api to mark message as seen using message id
+
+export const markMessageAsSeen = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id,{seen:true})
+        res.json({success:true})
+
+    }catch(error){
+         console.log(error.message)
+        res.json({success:false,error:error.message})
+
+
+    }
+
 }

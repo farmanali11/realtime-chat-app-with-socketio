e9916367dd1558dd0185947868731e5fc009db30
farmanali11 <farmanalishar11@gmail.com>
2025-07-04 18:55:05 +0500

    Fix missing bgImage.svg reference

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage";
@@ -9,35 +9,34 @@ import ProfilePage from "./pages/ProfilePage";
 import { AuthContext } from "../context/AuthContext";
 
 const App = () => {
-  // Get authentication state from context
   const { authUser } = useContext(AuthContext);
 
   return (
-    <div
-      className="bg-[url('/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain min-h-screen">
       {/* Global toast notification system */}
       <Toaster position="top-center" reverseOrder={false} />
 
-      {/* Routing configuration */}
-      <Routes>
-        {/* Home route - protected */}
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
+      <BrowserRouter>
+        <Routes>
+          {/* Home route - protected */}
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
 
-        {/* Login route - accessible only if not logged in */}
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
+          {/* Login route - accessible only if not logged in */}
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
 
-        {/* Profile route - protected */}
-        <Route
-          path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-      </Routes>
+          {/* Profile route - protected */}
+          <Route
+            path="/profile"
+            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };

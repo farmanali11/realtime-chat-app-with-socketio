fd476a392eca9d371e164c67954e5ed7af6ade87
farmanali11 <farmanalishar11@gmail.com>
2025-07-04 19:09:57 +0500

    Added the Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage";
@@ -16,27 +16,25 @@ const App = () => {
       {/* Global toast notification system */}
       <Toaster position="top-center" reverseOrder={false} />
 
-      <BrowserRouter>
-        <Routes>
-          {/* Home route - protected */}
-          <Route
-            path="/"
-            element={authUser ? <HomePage /> : <Navigate to="/login" />}
-          />
+      <Routes>
+        {/* Home route - protected */}
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
 
-          {/* Login route - accessible only if not logged in */}
-          <Route
-            path="/login"
-            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-          />
+        {/* Login route - accessible only if not logged in */}
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
 
-          {/* Profile route - protected */}
-          <Route
-            path="/profile"
-            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-          />
-        </Routes>
-      </BrowserRouter>
+        {/* Profile route - protected */}
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </div>
   );
 };
